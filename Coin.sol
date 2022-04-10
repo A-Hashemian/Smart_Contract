6f7ba204be2e9560d9db4c9beff79089d0b48a4d
A-Hashemian <amin@hashemian.tech>
2022-04-10 22:36:45 +0300

    first commit

---
+++ Coin.sol
@@ -0,0 +1,14 @@
+pragma solidity 0.4.8;
+
+contract Coin {
+/*
+* @title A Simple Subcurrency Example
+* @author Amin Hashemian
+* 
+* @dev This is only for demo the simple Coin example
+*
+*/
+address public minter;
+uint public totalCoins;
+
+}
