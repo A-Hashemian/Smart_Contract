cef2da0371ce48ae609b36051f87cfb155ea91f7
A-Hashemian <amin@hashemian.tech>
2022-11-21 20:49:27 +0300

    ballot.sol

--- Ballot.sol
+++ Ballot.sol
@@ -31,4 +31,23 @@ contract Ballot {
         }
     }
 
+ function delegate(address to) public {
+        Voter storage sender = voters[msg.sender];
+        require(!sender.voted, "You already voted.");
+        require(to != msg.sender, "Self-delegation is disallowed.");
+
+        while (voters[to].delegate != address(0)) {
+            to = voters[to].delegate;
+            require(to != msg.sender, "Found loop in delegation.");
+        }
+        sender.voted = true;
+        sender.delegate = to;
+        Voter storage delegate_ = voters[to];
+        if (delegate_.voted) {
+            proposals[delegate_.vote].voteCount += sender.weight;
+        } else {
+            delegate_.weight += sender.weight;
+        }
+    }
+
 }
