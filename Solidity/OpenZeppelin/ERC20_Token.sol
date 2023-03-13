fb3c51702661633db507137c840f6c7d693cbd83
A-Hashemian <90988199+A-Hashemian@users.noreply.github.com>
2023-03-13 15:53:38 -0400

    Update ERC20_Token.sol

--- Solidity/OpenZeppelin/ERC20_Token.sol
+++ Solidity/OpenZeppelin/ERC20_Token.sol
@@ -1,1 +1,12 @@
 
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+
+//import OpenZeppelin libraries
+
+contract MyToken is Ownable, ERC20 {}
+// Creates a contract called MyToken that inherits from the Ownable and ERC20 libraries
