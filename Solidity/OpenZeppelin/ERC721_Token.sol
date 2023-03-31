29ff80d81de170df20bfc3900e54b702e22c5857
A-Hashemian <90988199+A-Hashemian@users.noreply.github.com>
2023-03-31 22:06:26 +0300

    Update ERC721_Token.sol

--- Solidity/OpenZeppelin/ERC721_Token.sol
+++ Solidity/OpenZeppelin/ERC721_Token.sol
@@ -2,6 +2,9 @@ pragma solidity ^0.5.2;
 import "openzeppelin-solidity/contracts/token/ERC721/ERC721Metadata.sol";
 contract Condos is ERC721Metadata {
 
+
+/*  Id is a public variable used to hold the token ID
+and MDTrack is a public hashmap that maps addresses to token IDs, which are of the integer type */
 uint32 public Id;
 mapping(address => uint32) public MDTrack;
 
