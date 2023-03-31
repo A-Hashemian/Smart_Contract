6a559732aa3e944b23b20d7651dd1025fdc876b7
A-Hashemian <90988199+A-Hashemian@users.noreply.github.com>
2023-03-31 18:04:13 +0300

    Update ERC721_Token.sol

--- Solidity/OpenZeppelin/ERC721_Token.sol
+++ Solidity/OpenZeppelin/ERC721_Token.sol
@@ -2,6 +2,8 @@ pragma solidity ^0.5.2;
 import "openzeppelin-solidity/contracts/token/ERC721/ERC721Metadata.sol";
 contract Condos is ERC721Metadata {
 
+uint32 public Id;
+mapping(address => uint32) public MDTrack;
 
 
 
