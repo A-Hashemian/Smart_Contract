64c0b04bb9cc60d467c973b30f22b6b57c8c9565
A-Hashemian <90988199+A-Hashemian@users.noreply.github.com>
2023-04-13 23:57:09 +0300

    Update ERC1155_Token.sol

--- Solidity/OpenZeppelin/ERC1155_Token.sol
+++ Solidity/OpenZeppelin/ERC1155_Token.sol
@@ -1,1 +1,12 @@
 
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import "@openzeppelin/contracts/utils/Context.sol";
+
+contract MyToken is ERC1155, Context {
+    constructor() ERC1155("https://example.com/token/{id}.json") {
+        _mint(_msgSender(), 1, 100, "");
+        _mint(_msgSender(), 2, 50, "");
+    }
