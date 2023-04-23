5fa909aab1f488ba2784761704f2b03b4c75d8a5
A-Hashemian <90988199+A-Hashemian@users.noreply.github.com>
2023-04-23 23:45:30 +0300

    Update ERC777_Token.sol

--- Solidity/OpenZeppelin/ERC777_Token.sol
+++ Solidity/OpenZeppelin/ERC777_Token.sol
@@ -1,1 +1,5 @@
 
+pragma solidity ^0.8.0;
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.3.1/contracts/token/ERC777/ERC777.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.3.1/contracts/token/ERC777/IERC777Recipient.sol";
