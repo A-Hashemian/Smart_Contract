405f7347ff0957154b46c2097a2f89edebaf5561
A-Hashemian <90988199+A-Hashemian@users.noreply.github.com>
2023-04-24 23:08:32 +0300

    Update ERC1400_Token.sol

--- Solidity/OpenZeppelin/ERC1400_Token.sol
+++ Solidity/OpenZeppelin/ERC1400_Token.sol
@@ -1,1 +1,6 @@
 
+pragma solidity ^0.8.0;
+
+// Import ERC-1400 token standard and Ownable contract from OpenZeppelin
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.3.1/contracts/token/ERC1400/ERC1400.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.3.1/contracts/access/Ownable.sol";
