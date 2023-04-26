a894f3705121fc373364f2aa1034a91239547c5d
A-Hashemian <90988199+A-Hashemian@users.noreply.github.com>
2023-04-26 23:33:10 +0300

    Update TRC20_Token.sol

--- Solidity/OpenZeppelin/TRC20_Token.sol
+++ Solidity/OpenZeppelin/TRC20_Token.sol
@@ -1,1 +1,9 @@
 
+pragma solidity ^0.5.8;
+
+contract TRC20Token {
+    string public name;          // The name of the token
+    string public symbol;        // The symbol of the token
+    uint8 public decimals = 18;  // The number of decimals, strongly suggested to be 18
+    uint256 public totalSupply;  // The total token supply
+}
