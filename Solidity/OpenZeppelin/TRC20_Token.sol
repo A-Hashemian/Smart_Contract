ae7100671d0590adae8fbaa081d91473779482e0
A-Hashemian <90988199+A-Hashemian@users.noreply.github.com>
2023-04-26 23:37:38 +0300

    Update TRC20_Token.sol

--- Solidity/OpenZeppelin/TRC20_Token.sol
+++ Solidity/OpenZeppelin/TRC20_Token.sol
@@ -9,4 +9,14 @@ contract TRC20Token {
     
     mapping (address => uint256) private _balances;  // Mapping of each address's token balance
     mapping (address => mapping (address => uint256)) private _allowed;  // Mapping of each address's approved spending limits
+    
+     constructor(uint256 initialSupply, string memory tokenName, string memory tokenSymbol) public {
+        // Set the initial token supply and allocate all tokens to the contract creator
+        totalSupply = initialSupply * 10 ** uint256(decimals);
+        _balances[msg.sender] = totalSupply;
+
+        // Set the token name and symbol
+        name = tokenName;
+        symbol = tokenSymbol;
+    }
 }
